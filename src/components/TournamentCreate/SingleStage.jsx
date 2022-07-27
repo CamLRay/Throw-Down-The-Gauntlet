39526852da2813c41ebea920bc45f5e1947cb55a
Sploob <crayiphone@gmail.com>
2022-07-26 19:39:00 -0700

    WIP: Style tournament create form

--- src/components/TournamentCreate/SingleStage.jsx
+++ src/components/TournamentCreate/SingleStage.jsx
@@ -4,8 +4,9 @@ function SingleStage(props) {
 const {setStyle} = props
 
   return (
-    <div>
-      <select onChange={(e)=>setStyle({groups: e.target.value, elim: null})} required>
+    <div className="flex justify-between p-2">
+      <h1 className='w-1/3'>Format</h1>
+      <select className='w-2/3 bg-gray-600' onChange={(e)=>setStyle({groups: e.target.value, elim: null})} required>
         <option value="koth">King of the Hill</option>
         {/* <option>Single Elimination</option>
         <option>Double Elimination</option> */}
--- src/components/TournamentCreate/TournamentCreate.jsx
+++ src/components/TournamentCreate/TournamentCreate.jsx
@@ -19,10 +19,6 @@ const TournamentCreate = (props) =>{
   const {user} = useAuth();
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    console.log(style)
-  },[style])
-
   const onSubmit = async(e) =>{
     e.preventDefault();
     if(user){
@@ -47,43 +43,59 @@ const TournamentCreate = (props) =>{
 
   const handleCategoryAdd = () => {
     setCategories([...categories, categoryToAdd])
-    console.log("click")
   }
 
   return(
     <>
-    <form onSubmit={onSubmit}>
-      <div>
-        <h3 className="bg-slate-600">Info</h3>
-        <label>Tournament name</label>
-        <input type='text' placeholder="Tournament Name" onChange={(e)=>setName(e.target.value)} required/>
-        <label>Description</label>
-        <textarea onChange={(e)=>setDescription(e.target.value)} required/>
-      </div>
-      <div>
-        <h3>Rules</h3>
-        <div>
-        <div>
-          <label>Round Length</label>
-          <input type="number" placeholder="Minutes" min={0} max={99} onChange={(e)=>setLength(e.target.value)} required/>
+    <form className="flex-col m-10 text-white" onSubmit={onSubmit} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}>
+      <div className="bg-slate-700 flex flex-col rounded mb-5">
+        <h3 className="bg-slate-800 p-1 pl-2">Info</h3>
+        <div className="flex justify-between p-2">
+          <label className="w-1/3">Tournament name</label>
+          <input className="w-2/3 bg-gray-600 p-1 rounded" type='text' placeholder="Tournament Name" onChange={(e)=>setName(e.target.value)} required/>
         </div>
-          <label>Categories</label>
-          <ol>
-            {categories.map((category)=> {
-            return <li key={v4()}>{category}</li>})}
-          </ol>
-
-          <input type='text' placeholder="Category" onChange={(e)=>setCategoryToAdd(e.target.value)}/>
-          <button type='button' onClick={()=>handleCategoryAdd()}>Add Category</button>
-          <input type='radio' id="single" name="torny_type" value={false} onChange={()=>{setIsTwoStage(false)}} />
-          <label htmlFor='single' checked='checked'>Single Stage</label>
-          <input type='radio' id="two" name="torny_type" value={true} onChange={()=>{setIsTwoStage(true)}} />
-          <label htmlFor='two'>Two Stage</label>
+        <div className="flex justify-between p-2">
+          <label>Description</label>
+          <textarea className='w-2/3 h-40 bg-gray-600 p-1 rounded' onChange={(e)=>setDescription(e.target.value)} required/>
         </div>
-        <label>Format</label>
-        {isTwoStage ? <TwoStage setStyle={[setStyle]} /> : <SingleStage setTyle={[setStyle]} />}
       </div>
-      <button>Create Tournament</button>
+      <div className="bg-slate-700 flex flex-col rounded mb-5">
+        <h3 className="bg-slate-800 p-1 pl-2">Rules</h3>
+        <div>
+          <div className="flex justify-between p-2">
+            <label className="w-1/3">Round Length</label>
+            <input className="w-2/3 bg-gray-600 p-1 rounded" type="number" placeholder="Minutes" min={0} max={99} onChange={(e)=>setLength(e.target.value)} required/>
+          </div>
+          <label className="p-2">Categories</label>
+          <div className="flex justify-between p-2">
+            <div className="w-1/3 bg-gray-600 p-2 rounded">
+              <input className="bg-gray-700 p-1 rounded mb-1" type='text' placeholder="Category" onChange={(e)=>setCategoryToAdd(e.target.value)}/>
+              <button className="bg-amber-600 p-1 rounded" type='button' onClick={()=>handleCategoryAdd()}>Add Category</button>
+              <button className="bg-amber-900 p-1 rounded ml-1" type="button" onClick={()=>setCategories([])}>Clear List</button>
+            </div>
+            <ol className="bg-gray-800 p-2 w-2/3 flex flex-col flex-wrap overflow-auto justify-start h-40">
+              {categories.length ? categories.map((category)=> {
+              return <li className="pr-2" key={v4()}>{category}</li>}): <li className="text-white/50 text-center text-4xl">Empty</li>}
+            </ol>
+          </div>
+            
+            <div className="flex justify-center">
+              <div className="p-1">
+                <input type='radio' id="single" name="torny_type" value={false} onChange={()=>{setIsTwoStage(false)}} />
+                <label htmlFor='single' checked='checked'>Single Stage</label>
+              </div>
+              <div className="p-1">
+                <input type='radio' id="two" name="torny_type" value={true} onChange={()=>{setIsTwoStage(true)}} />
+                <label htmlFor='two'>Two Stage</label>
+              </div>
+            </div>
+          
+        </div>
+        <div>
+          {isTwoStage ? <TwoStage setStyle={[setStyle]} /> : <SingleStage setTyle={[setStyle]} />}
+        </div>
+      </div>
+      <button className="bg-amber-600 p-1 font-semibold rounded">Create Tournament</button>
     </form>
     </>
   );
