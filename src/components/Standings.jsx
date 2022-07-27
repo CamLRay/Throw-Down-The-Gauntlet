a83e4b32a9d82f8aa1772005eff300f281162fff
Sploob <crayiphone@gmail.com>
2022-07-27 16:44:09 -0700

    style standings module

--- src/components/Standings.jsx
+++ src/components/Standings.jsx
@@ -6,25 +6,25 @@ function Standings() {
   const [details] = useOutletContext()
   return (
     <div className='flex justify-center'>
-      <table className='m-2 mx-10 auto'>
+      <table className='m-2 mx-10 w-2/3 text-center'>
         <thead>
           <tr>
             <th>#</th>
             <th>Name </th>
             <th>Persona</th>
             <th>Wins</th>
-            <th>Win Streaks</th>
+            <th>Top Streaks</th>
           </tr>
         </thead>
         <tbody>
           {details.players.sort((a, b)=>(a.totalCount < b.totalCount) ? 1 : ((b.totalCount < a.totalCount) ? -1 : 0)).map((player,i)=>{
             return(
-              <tr key={player.id} className={`text-white p-1 ${i%2 ? "bg-slate-500" : "bg-slate-600"}`}>
-                <td>{i+1}</td>
-                <td>{player.name}</td> 
-                <td>{player.persona}</td>
-                <td>{player.totalCount}</td>
-                <td>{player.history.map((streak)=>{return streak + " "})}</td>
+              <tr key={player.id} className={`text-white ${i%2 ? "bg-slate-500" : "bg-slate-600"}`}>
+                <td className='p-1'>{i+1}</td>
+                <td className='p-1'>{player.name}</td> 
+                <td className='p-1'>{player.persona}</td>
+                <td className='p-1'>{player.totalCount}</td>
+                <td className='p-1'>{player.history.map((streak)=>{return streak + " "})}</td>
               </tr>
             ) 
           })}
