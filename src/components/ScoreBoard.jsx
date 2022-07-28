f559af6606d41b16fad62a5df1d854fecfc790af
Sploob <crayiphone@gmail.com>
2022-07-28 13:14:44 -0700

    make podium responsive

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -10,31 +10,35 @@ const ScoreBoard = (props) =>{
   return(
     
     <>
-    <div className="flex justify-center">
-      <div className="bg-neutral-500 h-12 w-1/6 flex-col mt-4">
-      {props.persona ? sortCounters()[1].persona + " " + sortCounters()[1].totalCount + ' wins' : sortCounters()[1].name + " " + sortCounters()[1].totalCount + ' wins' }
-      <div className="bg-neutral-700 h-12 w-full">2nd</div>
-      </div>
-      <div className="bg-neutral-500 h-18 w-1/6">
-      {props.persona ?  sortCounters()[0].persona + " " + sortCounters()[0].totalCount + ' wins': sortCounters()[0].name + " " + sortCounters()[0].totalCount + ' wins'}
-      <div className="bg-neutral-700 h-16 w-full">1st</div>
-      </div>
-      <div className="bg-neutral-500 h-10 w-1/6 mt-6">
-      {props.persona ? sortCounters()[2].persona + " " + sortCounters()[2].totalCount + ' wins' : sortCounters()[2].name + " " + sortCounters()[2].totalCount + ' wins'}
-      <div className="bg-neutral-700 h-10 w-full">3rd</div>
+    <div className="hidden sm:block">
+      <div className="flex justify-center">
+        <div className="bg-neutral-500 h-12 w-1/6 flex-col mt-4">
+        {props.persona ? sortCounters()[1].persona + " " + sortCounters()[1].totalCount + ' wins' : sortCounters()[1].name + " " + sortCounters()[1].totalCount + ' wins' }
+        <div className="bg-neutral-700 h-12 w-full">2nd</div>
+        </div>
+        <div className="bg-neutral-500 h-18 w-1/6">
+        {props.persona ?  sortCounters()[0].persona + " " + sortCounters()[0].totalCount + ' wins': sortCounters()[0].name + " " + sortCounters()[0].totalCount + ' wins'}
+        <div className="bg-neutral-700 h-16 w-full">1st</div>
+        </div>
+        <div className="bg-neutral-500 h-10 w-1/6 mt-6">
+        {props.persona ? sortCounters()[2].persona + " " + sortCounters()[2].totalCount + ' wins' : sortCounters()[2].name + " " + sortCounters()[2].totalCount + ' wins'}
+        <div className="bg-neutral-700 h-10 w-full">3rd</div>
+        </div>
       </div>
     </div>
-    
-    {/* {sortCounters().map((player,i)=>{
-      
-      return( i < 3 ?
-        <div key={v4()} className="">
-          <p>#{i + 1} {player.name} {player.totalCount} wins</p>
-        </div>
-        : null
-      ) 
-      
-    })} */}
+   <div className="sm:hidden">
+     
+     {sortCounters().map((player,i)=>{
+     
+       return( i < 3 ?
+         <div key={v4()} className="">
+           <p>#{i + 1} {player.name} {player.totalCount} wins</p>
+         </div>
+         : null
+       )
+     
+     })}
+   </div>
     </>
     
     )
